0036a0154c224fea007577c17bed8ae78d8cef92
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-28 21:22:49 +0200

    display to the priest how many followers do they have

--- resources/views/dashboard/priest/index.blade.php
+++ resources/views/dashboard/priest/index.blade.php
@@ -9,7 +9,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <h2>Twoi followersi ({{ $followers->count() }})</h2>
+                    <ul>
+                        @forelse ($followers as $follower)
+                            <li>
+                                {{ $follower->name }}
+                            </li>
+                        @empty
+                            <li>Nikt się jeszcze nie zapisał</li>
+                        @endforelse
+                    </ul>
                 </div>
             </div>
         </div>
