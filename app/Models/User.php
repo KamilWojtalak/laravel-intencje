5febe1667979bc4a16a5b88e97814d2407f678a2
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-28 19:43:10 +0200

    add getPriests method

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Contracts\Database\Query\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Collection;
 
 class User extends Authenticatable
 {
@@ -49,5 +52,14 @@ class User extends Authenticatable
     public function roles(): BelongsToMany
     {
         return $this->belongsToMany(Role::class, 'role_users');
+    }
+
+    public static function getPriests(): Collection
+    {
+        return User::query()
+            ->whereHas('roles', function(Builder $q) {
+                $q->where('name', Role::ROLE_PARISH);
+            })
+            ->get();
     }
 }
