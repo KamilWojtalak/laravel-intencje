aa9d828429b19cc8a7717093e9231cd41e2ee39d
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-28 20:05:22 +0200

    define columns at priest_user table migraiton

--- database/migrations/2024_05_28_180422_create_priest_user_table.php
+++ database/migrations/2024_05_28_180422_create_priest_user_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('priest_user', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->unsignedBigInteger('priest_id');
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('priest_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
