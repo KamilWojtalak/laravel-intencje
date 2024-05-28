d4593be6094f7e21397739e2a51e423c0b71e8c8
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-28 19:50:38 +0200

    add assign priest form at root route

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -1,3 +1,14 @@
 <x-public-layout>
     wyszukaj parafie
+
+    <form action="{{ route('register') }}" method="get">
+        <select name="priest_id" id="priest_id">
+            @foreach ($priests as $priest)
+                <option value="{{ $priest->id }}">{{ $priest->name }}</option>
+            @endforeach
+        </select>
+
+        <button>Zapisz się</button>
+    </form>
+
 </x-public-layout>
