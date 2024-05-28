7db14a3794222e12a08b911003f7386f5ca2a224
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-28 19:25:36 +0200

    add 2 register links

--- resources/views/layouts/navigation/public.blade.php
+++ resources/views/layouts/navigation/public.blade.php
@@ -1,7 +1,10 @@
 <nav>
     <ul>
         <li>
-            <a href="{{ route('register') }}">Register</a>
+            <a href="{{ route('register-priest') }}">Register proboszcz</a>
+        </li>
+        <li>
+            <a href="{{ route('register') }}">Register user</a>
         </li>
         <li>Login</li>
         <li>Logout</li>
