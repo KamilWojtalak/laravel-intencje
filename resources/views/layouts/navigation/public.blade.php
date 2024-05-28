2b61ef45c739f15f2037b3775e7de8216d8208e0
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-28 20:33:57 +0200

    guest and auth navigation

--- resources/views/layouts/navigation/public.blade.php
+++ resources/views/layouts/navigation/public.blade.php
@@ -1,12 +1,20 @@
 <nav>
     <ul>
-        <li>
-            <a href="{{ route('register-priest') }}">Register proboszcz</a>
-        </li>
-        <li>
-            <a href="{{ route('register') }}">Register user</a>
-        </li>
-        <li>Login</li>
-        <li>Logout</li>
+        @guest
+            <li>
+                <a href="{{ route('register-priest') }}">Register proboszcz</a>
+            </li>
+            <li>
+                <a href="{{ route('register') }}">Register user</a>
+            </li>
+            <li>
+                <a href="{{ route('login') }}">Login</a>
+            </li>
+        @endguest
+        @auth
+            <li>
+                <a href="{{ route('logout') }}">Logout</a>
+            </li>
+        @endauth
     </ul>
 </nav>
