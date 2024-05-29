a0ec98bdd8f9f7152a26b027f2ff7bd103d85525
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-29 06:16:36 +0200

    test calendar js

--- resources/views/dashboard/priest/index.blade.php
+++ resources/views/dashboard/priest/index.blade.php
@@ -1,3 +1,15 @@
+@push('head_scripts')
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.13/index.global.min.js'></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var calendarEl = document.getElementById('calendar');
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth'
+            });
+            calendar.render();
+        });
+    </script>
+@endpush
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -39,6 +51,9 @@
                         @endforelse
                     </ul>
                 </div>
+
+                <div id="calendar"></div>
+
             </div>
         </div>
     </div>
