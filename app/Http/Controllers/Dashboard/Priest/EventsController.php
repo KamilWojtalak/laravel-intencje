f4bf52d3621f96a75ff1bb3b11350c6dfd33af2c
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-29 18:56:26 +0200

    add create method to evets controller

--- app/Http/Controllers/Dashboard/Priest/EventsController.php
+++ app/Http/Controllers/Dashboard/Priest/EventsController.php
@@ -1,10 +1,14 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Dashboard\Priest;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class EventsController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('dashboard.priest.events.create');
+    }
 }
