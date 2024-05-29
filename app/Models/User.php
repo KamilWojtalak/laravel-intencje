485ace8b0cb5ca49f254421e9047e2f66f46a124
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-29 05:20:22 +0200

    modify table name in relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,12 +59,12 @@ class User extends Authenticatable
 
     public function prists(): BelongsToMany
     {
-        return $this->belongsToMany(User::class, 'priest_user', 'follower_id', 'priest_id');
+        return $this->belongsToMany(User::class, 'priest_follower', 'follower_id', 'priest_id');
     }
 
     public function followers(): BelongsToMany
     {
-        return $this->belongsToMany(User::class, 'priest_user', 'priest_id', 'follower_id');
+        return $this->belongsToMany(User::class, 'priest_follower', 'priest_id', 'follower_id');
     }
 
     public static function getPriests(): Collection
