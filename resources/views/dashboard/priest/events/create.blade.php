6c321d70bf9e3a19f1b7d00aafda9f358931d3e8
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-29 19:03:11 +0200

    create event form

--- resources/views/dashboard/priest/events/create.blade.php
+++ resources/views/dashboard/priest/events/create.blade.php
@@ -9,6 +9,28 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
 
+                <form action="{{ route('dashboard.priest.events.store') }}" method="post">
+                    @csrf
+                    <div class="">
+                        Name
+                        <input type="name" name="name">
+                    </div>
+                    <div class="">
+                        Dzień
+                        <input type="date" name="" id="">
+                    </div>
+                    <div class="">
+                        Godzina
+                        <input type="number" name="hourd" id="hourd">
+                    </div>
+                    <div class="">
+                        Minuty
+                        <input type="minutes" name="minutes" id="minutes">
+                    </div>
+
+                    <button>Stwórz mszę</button>
+                </form>
+
             </div>
         </div>
     </div>
