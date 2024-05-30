6d839f70d1dcfd8a7c415e3f49fe6b7059e62930
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-30 05:48:43 +0200

    add sample test data to the form

--- resources/views/dashboard/follower/events/create.blade.php
+++ resources/views/dashboard/follower/events/create.blade.php
@@ -10,6 +10,25 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
 
+                    <h3>Dane o eventcie</h3>
+                    <p>Nazwa: {{ $event->name }}</p>
+                    <p>Kiedy: {{ $event->start_at->diffForHumans() }}</p>
+
+                    <form action="{{ route('dashboard.follower.events.store') }}" method="post">
+                        @csrf
+                        <div class="">
+                            <textarea name="message" id="message" cols="30" rows="10" placeholder="Tekst twojej intencji">Test, przykładowy tekst intencji za Wojciecha Bogacza</textarea>
+                        </div>
+
+                        <div class="">
+                            <p>ile chcesz przekazać pieniędzy: (co łaska, min. 2 bo dostarczyciel płatności) (zł)</p>
+                            <input type="number" name="price" id="price" min="2" value="2"> (zł)
+                        </div>
+
+                        <div class="">
+                            <button>Przejdź do płatności</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
