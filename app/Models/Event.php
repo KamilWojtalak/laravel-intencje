4a508004b9ee6a5fef90e8fbd39d7c9c4f3daa0f
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-30 06:52:22 +0200

    add participants relationship

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Support\Collection;
 
 class Event extends Model
@@ -15,6 +16,18 @@ class Event extends Model
     protected $casts = [
         'start_at' => 'datetime'
     ];
+
+    public function participants(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            User::class,
+            'event_participant',
+            'event_id',
+            'user_id',
+            'id',
+            'id',
+        );
+    }
 
     public static function getByPriest(User $priest): Collection
     {
