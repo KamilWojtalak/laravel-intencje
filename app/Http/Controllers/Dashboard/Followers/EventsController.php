866142be97b77a76775d30fe7894bd378f06c1d6
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-30 06:46:41 +0200

    cs

--- app/Http/Controllers/Dashboard/Followers/EventsController.php
+++ app/Http/Controllers/Dashboard/Followers/EventsController.php
@@ -36,7 +36,7 @@ class EventsController extends Controller
          * TODO na razie bez płatności dodaj
          */
 
-         dd('jeszcze nie implemented');
+        dd('jeszcze nie implemented');
         //  TODO do dodania participants relationship i table
         $event->participants()->create([
             'user_id' => auth()->id(),
