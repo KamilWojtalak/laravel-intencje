a1384a255e590c8b1af4078de549762988356219
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-06-01 08:52:01 +0200

    remove invalid url from menu

--- resources/views/layouts/navigation/public.blade.php
+++ resources/views/layouts/navigation/public.blade.php
@@ -5,9 +5,6 @@
                 <a href="{{ route('register-priest') }}">Register proboszcz</a>
             </li>
             <li>
-                <a href="{{ route('register') }}">Register user</a>
-            </li>
-            <li>
                 <a href="{{ route('login') }}">Login</a>
             </li>
         @endguest
