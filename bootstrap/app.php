4e7b87651bd2e5a978d32f6c6319fed94aa2b416
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-06-02 13:33:29 +0200

    add stripe route to csrf

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
             'payments/przelewy24/status',
+            'payments/stripe/status',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
