17adba8e526c4b29e6de1bf63fea51ffa00da013
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-06-02 13:32:42 +0200

    add stripe constants

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -9,6 +9,9 @@ class Payment extends Model
 {
     use HasFactory;
 
+    const PROVIDER_STRIPE = 'stripe';
+    const PROVIDER_PRZELEWY24 = 'przelewy24';
+
     const STATUS_INIT = 'init';
     const STATUS_VERIFIED = 'verified';
 
